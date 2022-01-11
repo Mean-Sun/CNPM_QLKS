f56b3daed673d29bcf188180f9f8f3b2cb1605d0
dhieu1304 <d.hieu.13.04@gmail.com>
2022-01-12 02:48:47 +0700

    update create staff

--- routes/admin/staff.js
+++ routes/admin/staff.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const databaseConfig = require('../../models/db');
 const fs = require('fs');
+const bcrypt = require("bcrypt");
 
 // Danh sách nhân viên
 router.get('/', function (req, res, next) {
@@ -34,6 +35,7 @@ router.get('/create', function (req, res, next) {
 // add staff
 router.post('/create', function (req, res, next) {
     let staff = {
+        MaNV: 0,
         TenNV: req.body.TenNV,
         DiaChi: req.body.DiaChi,
         NgaySinh: req.body.NgaySinh,
@@ -84,71 +86,40 @@ router.post('/create', function (req, res, next) {
         });
     }
 
-    databaseConfig.query('INSERT INTO nhanvien SET ?', staff, function (err, result) {
+    let sql = `SELECT * FROM nhanvien WHERE email = "` + staff.email + `"`;
+    databaseConfig.query(sql, function (err, rows) {
         if (err) {
-            message = 'Đã xảy ra lỗi, vui lòng kiểm tra lại dữ liệu!!!'
-            req.flash('error', err)
-            console.log(err)
-            // render to add.ejs
-            res.render('admin/staff/create', {
-                staff,
-                message,
-                layout: 'orther',
-            })
+            req.flash('error', err);
         } else {
-            req.flash('success', 'Product successfully added');
-            res.redirect('/staff/');
-        }
-    })
-
-
-    /*let MaNV = req.body.MaNV;
-    let TenNV = req.body.TenNV;
-    let DiaChi = req.body.DiaChi;
-    let NgaySinh = req.body.NgaySinh;
-    let SDT = req.body.SDT;
-    let email = req.body.email;
-    let MatKhau = req.body.MatKhau;
-    const bcrypt = require("bcrypt");        
-    var salt = bcrypt.genSaltSync(10);
-    var pass_mahoa = bcrypt.hashSync(MatKhau, salt);
-    let Role = req.body.Role;
-
-    let errors = false;
-    if (!errors) {
-        var form_data = {
-            MaNV: MaNV,
-            TenNV: TenNV,
-            DiaChi: DiaChi,
-            NgaySinh: NgaySinh,
-            SDT: SDT,
-            email: email,
-            MatKhau: pass_mahoa,
-            Role: Role,
-        }
-        databaseConfig.query('INSERT INTO nhanvien SET ?', form_data, function (err, result) {
-            if (err) {
-                // console.log(form_data.image);
-                req.flash('error', err)
-                console.log(err)
-                // render to add.ejs
-                res.render('admin/staff/create', {
-                    MaNV: form_data.MaNV,
-                    TenNV: form_data.TenNV,
-                    DiaChi: form_data.DiaChi,
-                    NgaySinh: form_data.NgaySinh,
-                    SDT:form_data.SDT,
-                    email:form_data.email,
-                    MatKhau:form_data.pass_mahoa,
-                    Role:form_data.Role,
-                    layout: 'orther',
-                })
-            } else {
-                req.flash('success', 'Product successfully added');
-                res.redirect('/staff/');
+            if (rows.length == 1) {
+                message = 'Email này đã được sử dụng, vui lòng dùng email khác';
+                return res.render('admin/staff/create', {
+                    message,
+                    layout: 'orther'
+                });
             }
-        })
-    }*/
+            sql = `SELECT MaNV FROM nhanvien ORDER BY MaNV DESC LIMIT 0, 1`;
+            databaseConfig.query(sql, function (err, rows) {
+                if (err) {
+                    req.flash('error', err);
+                } else {
+                    staff.MaNV = rows[0].MaNV + 1;
+                    bcrypt.hash(staff.MatKhau, 10, function(err, hash) {
+                        //console.log(hash);
+                        staff.MatKhau = hash;
+                        databaseConfig.query('INSERT INTO nhanvien SET ?', staff, function (err, result) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                req.flash('success', 'Product successfully added');
+                                res.redirect('/staff/');
+                            }
+                        })
+                    });
+                }
+            });
+        }
+    });
 })
 
 // get view edit
@@ -170,13 +141,14 @@ router.get('/edit/(:MaNV)', function (req, res, next) {
                 email: rows[0].email,
                 MatKhau: rows[0].MatKhau,
                 Role: rows[0].Role,
+                isDel: rows[0].isDel,
                 layout:'orther'
             })
         }
     })
 })
 
-// Sửa Phòng
+// edit staff
 router.post('/edit/:MaNV',function(req,res,next){
     let MaNV = req.body.MaNV;
     let TenNV = req.body.TenNV;
@@ -227,13 +199,12 @@ router.post('/edit/:MaNV',function(req,res,next){
 
 })
 
-// Xóa 
-
+// Xóa
 router.get('/delete/(:MaNV)', function (req, res, next) {
 
     let id = req.params.MaNV;
 
-    databaseConfig.query('DELETE FROM nhanvien WHERE MaNV = ' + id, function (err, result) {
+    databaseConfig.query('UPDATE nhanvien SET ? WHERE MaNV = ' + id, function (err, result) {
         //if(err) throw err
         if (err) {
             // set flash message
@@ -249,5 +220,4 @@ router.get('/delete/(:MaNV)', function (req, res, next) {
     })
 })
 
-
-module.exports=router
+module.exports = router;
